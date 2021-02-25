a85c0921048fd56fa3b7934a15ee434615fd1f11
Opilane TTHK <opilane@tthk.ee>
2021-02-25 17:23:13 +0200

    Add project files.

---
+++ FoodCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmeika.Koršun
+{
+    class FoodCreator
+    {
+        int mapWidth;
+        int mapHeight;
+        char sym;
+        ConsoleColor color;
+        Random random = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char sym,ConsoleColor color_)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+            color = color_;
+        }
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWidth - 2);
+            int y = random.Next(2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
---
+++ Params.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmeika.Koršun
+{
+    public class Params
+    {
+        private string resourcesFoler;
+
+        public Params()
+        {
+            var ind = Directory.GetCurrentDirectory().ToString()
+                .IndexOf("bin", StringComparison.Ordinal); // Получить индекс папки bin
+
+            string binFolder = Directory.GetCurrentDirectory().ToString().Substring(0, ind).ToString(); //Путь до указанной d инкесе папки
+
+            resourcesFoler = binFolder + "resources\\";
+        }
+        public string GetResourceFolder()
+        {
+            return resourcesFoler;
+        }
+    }
+}
